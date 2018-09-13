e7d31ba3d3b4c82798bef77f7e928ca013764c43
Kevin <kevin@rocher-dev.com>
2018-09-13 23:13:49 +0200

    v 0.12.4
    * Default to yes for another meta field.

--- bin/add_post_metas.sh
+++ bin/add_post_metas.sh
@@ -43,8 +43,8 @@ if [[ $add_post_metas != 'n' ]]; then
         f_nl="\/\*newline\*\/";
         field_content="${f_tab}\$fields['entityidentity_${field_id}'] = array(${f_nl}${f_tab}${f_tab}'box' => 'entityidentity_details',${f_nl}${f_tab}${f_tab}'name' => __('${field_name}', '${project_prefix}'),${f_nl}${f_tab}${f_tab}'type' => '${field_type}'${f_nl}${f_tab});${f_nl}#wputentitycreatorpostfields";
         wpuentitycreator_sed "s/#wputentitycreatorpostfields/${field_content}/g" "${mainfile}";
-        read -p "Add another meta field ? (y/N) " field_new;
-        if [[ $field_new != 'y' ]]; then
+        read -p "Add another meta field ? (Y/n) " field_new;
+        if [[ $field_new == 'n' ]]; then
             break;
         fi;
     done
