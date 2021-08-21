9c3b6e0c4ec8c72dec60df3b7e63f43afbcc60fe
Kevin <kevin@rocher-dev.com>
2021-08-21 21:14:44 +0200

    v 0.32.0
    - Custom ACF Blocks

--- bin/add_block_type.sh
+++ bin/add_block_type.sh
@@ -6,15 +6,65 @@
 
 add_block_type='';
 while [ -z $add_block_type ]; do
-    read -p "What block type do you need ? ([c]tatitle,[e]mpty,[r]epeater) : " add_block_type;
+    read -p "What block type do you need ? ([c]tatitle,[e]mpty,[r]epeater,c[u]stom) : " add_block_type;
+
+    if [[ "${add_block_type}" == "" ]];then
+        continue
+    fi;
+
+    # Add Header
+    cat "${SOURCEDIR}tpl/blocks/0-header.txt" >> "${mainfile}";
+    # Add Before
+    cat "${SOURCEDIR}tpl/blocks/0-before.txt" >> "${mainfile}";
+
+    # Type CTA-Title
     if [[ $add_block_type == 'c' || $add_block_type == 'ctatitle' ]]; then
-        cat "${SOURCEDIR}tpl/blocks/ctatitle.php" >> "${mainfile}";
+        cat "${SOURCEDIR}tpl/blocks/ctatitle.txt" >> "${mainfile}";
     fi;
-    if [[ $add_block_type == 'e' || $add_block_type == 'empty' ]]; then
-        cat "${SOURCEDIR}tpl/blocks/empty.php" >> "${mainfile}";
-    fi;
+    # Type Repeater
     if [[ $add_block_type == 'r' || $add_block_type == 'repeater' ]]; then
-        cat "${SOURCEDIR}tpl/blocks/repeater.php" >> "${mainfile}";
+        cat "${SOURCEDIR}tpl/blocks/repeater.txt" >> "${mainfile}";
     fi;
+    # Type custom
+    if [[ $add_block_type == 'u' || $add_block_type == 'custom' ]]; then
+        continue_add_block_custom_value='1';
+        while [ ! -z $continue_add_block_custom_value ]; do
+            add_block_custom_value_type='';
+            while [ -z $add_block_custom_value_type ]; do
+                read -p "What field type ? ([t]itle,[i]mage) : " add_block_custom_value_type;
+
+                if [[ "${add_block_custom_value_type}" == "" ]];then
+                    echo '- Empty field type, ignoring this field.'
+                    continue;
+                fi;
+                read -p "What is the ID of the field ? : " add_block_custom_field_id;
+                if [[ "${add_block_custom_field_id}" == "" ]];then
+                    echo '- Empty field ID, ignoring this field.'
+                    continue;
+                fi;
+                add_block_custom_field_value="";
+
+                # Field type title
+                if [[ "${add_block_custom_value_type}" == 't' || "${add_block_custom_value_type}" == 'title' ]];then
+                    add_block_custom_field_value="'wpuacf_title'";
+                fi;
+                # Field type image
+                if [[ "${add_block_custom_value_type}" == 'i' || "${add_block_custom_value_type}" == 'image' ]];then
+                    add_block_custom_field_value="'wpuacf_image'";
+                fi;
+
+                if [[ "${add_block_custom_field_value}" != "" ]];then
+                    _indent_3="            ";
+                    echo "${_indent_3}'${add_block_custom_field_id}' => ${add_block_custom_field_value}," >> "${mainfile}";
+                fi;
+            done
+            read -p "Do you need another field ? Type Enter to stop or anything else to continue : " continue_add_block_custom_value;
+        done;
+
+    fi;
+    # Type Empty : no content needed
+
+    # Add after
+    cat "${SOURCEDIR}tpl/blocks/0-after.txt" >> "${mainfile}";
 done
 
--- wpuentityadd.sh
+++ wpuentityadd.sh
@@ -2,7 +2,7 @@
 
 echo '####';
 echo '#### WPU Entity Add';
-echo '#### v 0.31.0';
+echo '#### v 0.32.0';
 echo '####';
 echo '';
 
