aaeb735b0e5fea96b64ba74b9fdbc623df33cadc
Kevin <kevin@rocher-dev.com>
2025-03-29 23:01:16 +0100

    v 0.43.5
    - Added filter for 'wpuseo_metasldjson_single_type' to post type.

--- inc/add_wpuseo.php
+++ inc/add_wpuseo.php
@@ -8,3 +8,11 @@ add_filter('wpuseo_post_types', function ($pt) {
     $pt[] = 'entitypluralid';
     return $pt;
 }, 10, 1);
+
+
+add_filter('wpuseo_metasldjson_single_type', function ($name, $post_type) {
+    if ($post_type == 'entitypluralid') {
+        return 'entitynameentity';
+    }
+    return $name;
+}, 10, 2);
--- wpuentityadd.sh
+++ wpuentityadd.sh
@@ -2,7 +2,7 @@
 
 echo '####';
 echo '#### WPU Entity Add';
-echo '#### v 0.43.4';
+echo '#### v 0.43.5';
 echo '####';
 echo '';
 
